3202c2145a78a0a33672874ab6f93a520cf4574a
Kim Hee Soon/LGSI BDS <heesoon.kim@lge.com>
2024-12-26 11:16:11 +0530

    study of sklearn logistic regression package

--- ch03/LogisticRegressionSklearn.py
+++ ch03/LogisticRegressionSklearn.py
@@ -1,49 +1,10 @@
 import numpy as np
+from matplotlib.colors import ListedColormap
+import matplotlib.pyplot as plt
 from sklearn import datasets
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import accuracy_score
-from matplotlib.colors import ListedColormap
-import matplotlib.pyplot as plt
-
-class LogisticRegressionAGD(object):
-    def __init__(self, eta=0.05, n_iter=100, random_state=1):
-        self.eta = eta
-        self.n_iter = n_iter
-        self.random_state = 1
-        self.cost_ = []
-
-    def init_weight(self, X):
-        rgen = np.random.RandomState(self.random_state)
-        self.w_ = rgen.normal(loc=0.0, scale=0.01, size=1 + X.shape[1])
-
-    def update_weight(self, X, errors):
-        self.w_[1:] += self.eta * X.T.dot(errors)
-        self.w_[0] += self.eta * errors.sum()
-
-    def net_input(self, X):
-        return np.dot(X, self.w_[1:]) + self.w_[0]
-    
-    def activation(self, z):
-        return 1. / (1. + np.exp(-np.clip(z, -250, 250)))
-
-    def calcCost(self, y, output):
-        cost = -y.dot(np.log(output)) - ((1 - y).dot(np.log(1 - output)))
-        self.cost_.append(cost)
-
-    def fit(self, X, y):
-        self.init_weight(X)
-
-        for i in range(self.n_iter):
-            net_input = self.net_input(X)
-            output = self.activation(net_input)
-            errors = (y - output)
-            self.update_weight(X, errors=errors)
-            self.calcCost(y, output)
-        return self
-    
-    def predict(self, X):
-        return np.where(self.net_input(X) >= 0.0, 1, 0)
+from sklearn.linear_model import LogisticRegression
 
 def plot_decision_regions(X, y, classifier, test_idx=None, resolution=0.02):
     markers = ('s', 'x', 'o', '^', 'v')
@@ -91,12 +52,14 @@ def main():
     X_train_std = sc.transform(X_train)
     X_test_std = sc.transform(X_test)
 
-    X_train_01_subset = X_train_std[(y_train == 0) | (y_train == 1)]
-    y_train_01_subset = y_train[(y_train == 0) | (y_train == 1)]
-    
-    lrgd = LogisticRegressionAGD(eta=0.05, n_iter=1000, random_state=1)
-    lrgd.fit(X_train_01_subset, y_train_01_subset)
-    plot_decision_regions(X=X_train_01_subset, y=y_train_01_subset, classifier=lrgd)
+    X_combined_std = np.vstack((X_train_std, X_test_std))
+    y_combined = np.hstack((y_train, y_test))
+
+    lr = LogisticRegression(C=100.0, random_state=1)
+    lr.fit(X_train_std, y_train)
+
+    #plot_decision_regions(X_combined_std, y_combined, classifier=lr)
+    plot_decision_regions(X_combined_std, y_combined, classifier=lr, test_idx=range(105, 150))
 
 if __name__ == '__main__':
     main()
